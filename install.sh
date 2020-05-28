e23b0b2633bf9d865828d587797981eb3d9eac96
fz420 <fz420@qq.com>
2020-05-28 19:13:05 +0800

    git别名提示修复-去尾部空格

--- install.sh
+++ install.sh
@@ -131,18 +131,12 @@ alias ld="ll -d"
 alias c="clear"
 
 # https://githowto.com/aliases
-alias gs='git status '
-alias ga='git add '
-alias gb='git branch '
+alias gs='git status'
+alias ga='git add'
+alias gb='git branch'
 alias gc='git commit'
 alias gd='git diff'
-alias gco='git checkout '
-alias gk='gitk --all&'
-alias gx='gitx --all'
-
-alias got='git '
-alias get='git '
-
+alias gco='git checkout'
 
 # Then, source plugins and add commands to $PATH
 zplug load
